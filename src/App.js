5b8705b5233cfa0c10074e91cbd931a7dfafc1af
Park Road <stageroad0820@meoasis.com>
2024-09-03 17:38:13 +0900

    결제 시스템 유니티 테스트 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { View, Button, Vibration } from 'react-native';
-import { TonConnect} from '@tonconnect/sdk';
 import { TonConnectUI } from '@tonconnect/ui-react';
 
 
@@ -12,24 +11,23 @@ const connectorUi = new TonConnectUI({
   manifestUrl: 'https://lys-test.s3.ap-northeast-2.amazonaws.com/tonconnect-manifest.json'
 });
 
-export const App = () => {
-  window.Telegram.WebView.initParams = 7.7;
-
+const App = () => {
+  
   window.Telegram.WebApp.expand();
 
   const root = document.querySelector("#root");
- 
 
   const { unityProvider ,sendMessage ,addEventListener ,removeEventListener } = useUnityContext({
-    loaderUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Version37/Build.loader.js",
-    dataUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Version37/Build.data",
-    frameworkUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Version37/Build.framework.js",
-    codeUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Version37/Build.wasm",
+    loaderUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Payment1/Build.loader.js",
+    dataUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Payment1/Build.data",
+    frameworkUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Payment1/Build.framework.js",
+    codeUrl: "https://lys-test.s3.ap-northeast-2.amazonaws.com/Payment1/Build.wasm",
   });
 
   const TestUnityMessage = () => {
     sendMessage('SendReactManager' , 'ReciveUnity' , document.location.pathname)
   }
+
 
   const handleVibrate = () => {
     Vibration.vibrate(100); 
@@ -47,80 +45,72 @@ export const App = () => {
   const OpenUrl4 = () => {
     window.Telegram.WebApp.openLink("https://discord.com/invite/metaoasisvr");
   };
-   const OpenUrl5 = () => {
-    generatePayment("5000902684:AAGJqOsbMv7F5IPVexyVAAjVcMwV1y2yeTA", true, "테스트 결제용 아이템 이름", "테스트 결제용 아이템 설명입니다.", 10, `{"buyItemId":1}`);
+  const OpenUrl5 = () => {
+    window.Telegram.WebApp.openLink("https://www.meoasis.com/");
   };
 
-  /**
-   * Generate 'Telegram Stars' invoice link using Telegram API and send and receive invoice data to Telegram API Server
-   * @param {string} token Telegram Bot Token
-   * @param {boolean} isTest Is Telegram Test Server?
-   * @param {string} productTitle Item Title
-   * @param {string} productDescription Item Description
-   * @param {number} price  Item Price (Currency: XTR(Telegram Stars))
-   * @param {JSON} payload Item payload data (send to Telegram Bot with payload data)
-   */
-  function generatePayment(token, isTest, productTitle, productDescription, price, payload) {
-    var urlParams = {
-      "title": productTitle,
-      "description": productDescription,
-      "price": price,
-      "payload": payload
-    };
+  const Shop_Assistant = (str) =>{
+    //아래는 성공후 유니티로 보내는 방법 
+    //sendMessage('SendReactManager' , 'ReciveShopItem' ,아이템 번호);
+    OpenInvoiceAndPayment(str, 3);
+  };
+  
+  const Shop_Manager = (str) =>{
+    OpenInvoiceAndPayment(str, 4);
+  };
 
-    window.Telegram.WebApp.invokeCustomMethod('generatePaymentUrl', JSON.stringify(urlParams), event => {
-      console.log("봇 콜백 이벤트 실행됨: " + JSON.stringify(event));
+  const Shop_DieselTechnician = (str) =>{
+    OpenInvoiceAndPayment(str, 5);
+  };
+
+  const Shop_HarvestHelp = (str) =>{
+    OpenInvoiceAndPayment(str, 6);
+  };
+
+  const Shop_Farmer = (str) =>{
+    OpenInvoiceAndPayment(str, 7);
+  };
+
+  const Shop_CoinParty =(str)=>{
+    OpenInvoiceAndPayment(str, 2);
+  };
+
+  function OpenInvoiceAndPayment(url, itemNum) {
+    window.Telegram.WebApp.openInvoice(url, event => {
+      if (event.state !== 'paid') {
+        sendMessage('SendReactManager', 'ReciveShopItem', -1);
+
+      }
     });
 
-    /* var formatUrl = `https://api.telegram.org/bot${token}`;
+    window.Telegram.WebApp.onEvent('invoiceClosed', event => {
+      sendMessage('SendReactManager', 'ReciveShopItem', itemNum);
+    });
+  }
+  
+  const WalletConnect = () => {
+    GetWaleltConnect();
+  };
 
-    if (isTest) {
-        formatUrl = formatUrl + "/test";
+  async function GetWaleltConnect() {
+    if(connectorUi.connected){
+      connectorUi.disconnect();
     }
 
-    formatUrl = formatUrl + "/createInvoiceLink"
-    formatUrl = formatUrl + `?title=${productTitle}`;
-    formatUrl = formatUrl + `&description=${productDescription}`;
-    formatUrl = formatUrl + `&payload=${payload}`;
-    formatUrl = formatUrl + `&currency=XTR`;
-
-    const labeledPrices = [{"label":productTitle, "amount": price}];
-    formatUrl = formatUrl + `&prices=${JSON.stringify(labeledPrices)}`;
-
-    fetch(formatUrl)
-    .then(response => response.json())
-    .then(data => {
-      const result = data.result;
-      window.Telegram.WebApp.openInvoice(result, (event) => {
-        if (event.state !== 'paid') {
-          // TODO 결제가 완료되지 않았을 때 이벤트
-        }
-      });
-
-      window.Telegram.WebApp.onEvent('invoiceClosed', event => {
-        // TODO 결제가 완료되었을 때 이벤트
-        //console.log("event: " + JSON.stringify(event));
-      });
-    })
-    .catch(error => {
-      console.log(`An error occurred while purchasing in-game items: ${JSON.stringify(error)}`);
-    }); */
-  }
-
-  async function GetWalletAddress() {
     await connectorUi.openModal();
-    connectorUi.closeModal();
-
-    const currentState = connectorUi.modalState;
 
     const unsubscribe = connectorUi.onModalStateChange(
       state => {
-        alert(`[Modal (${currentState})]\nWallet Connected!\n- isConnected: ${connectorUi.connected}\n- wallet: ${connectorUi.wallet.provider}\n- account: ${connectorUi.account.address}\n- ${connectorUi.account.publicKey}`);
+        
+        sendMessage('SendReactManager' , 'ReciveWalletAddr' ,connectorUi.account.address);
+        connectorUi.closeModal();
+        unsubscribe();
       }
     );
-
-    unsubscribe();
+   
   }
+
+
 
   const handleCopyClipBoard = (text_s) => {
     //   navigator.clipboard.writeText(text_s);
@@ -148,6 +138,15 @@ export const App = () => {
     addEventListener('OpenUrl3',OpenUrl3);
     addEventListener('OpenUrl4',OpenUrl4);
     addEventListener('OpenUrl5',OpenUrl5);
+    addEventListener('WalletConnect',WalletConnect);
+
+    addEventListener('Shop_Assistant',Shop_Assistant);
+    addEventListener('Shop_Manager',Shop_Manager);
+    addEventListener('Shop_DieselTechnician',Shop_DieselTechnician);
+    addEventListener('Shop_HarvestHelp',Shop_HarvestHelp);
+    addEventListener('Shop_Farmer',Shop_Farmer);
+    addEventListener('Shop_CoinParty',Shop_CoinParty);
+
     return () => {
       removeEventListener('TakeTokenFromReact',TestUnityMessage)
       removeEventListener('MobileVibrate',handleVibrate)
@@ -157,14 +156,24 @@ export const App = () => {
       removeEventListener('OpenUrl3',OpenUrl3)
       removeEventListener('OpenUrl4',OpenUrl4)
       removeEventListener('OpenUrl5',OpenUrl5)
+      removeEventListener('WalletConnect',WalletConnect)
+
+      removeEventListener('Shop_Assistant',Shop_Assistant)
+      removeEventListener('Shop_Manager',Shop_Manager)
+      removeEventListener('Shop_DieselTechnician',Shop_DieselTechnician)
+      removeEventListener('Shop_HarvestHelp',Shop_HarvestHelp)
+      removeEventListener('Shop_Farmer',Shop_Farmer)
+      removeEventListener('Shop_CoinParty',Shop_CoinParty)
+
     }
 
-  },[addEventListener,removeEventListener,TestUnityMessage,handleCopyClipBoard,OpenUrl])
+  },[addEventListener,removeEventListener,TestUnityMessage,handleCopyClipBoard,OpenUrl,WalletConnect,
+    Shop_Assistant,Shop_Manager,Shop_DieselTechnician,Shop_HarvestHelp,Shop_Farmer,Shop_CoinParty,
+  ])
 
   return (
 
   <div className="App">
-
 
       <Unity
       devicePixelRatio={2}  
@@ -179,6 +188,7 @@ export const App = () => {
   </div>
 
   ) ;
+  
 };
 
 export default App; 
